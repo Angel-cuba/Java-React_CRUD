11cfa84d3330f70eb8573f1f1721e032e5086780
Mac_Developer <araozangel842@gmail.com>
2023-03-02 12:57:21 +0200

    Add user model

---
+++ src/main/java/com/crud/fullstackbackend/model/User.java
@@ -0,0 +1,51 @@
+package com.crud.fullstackbackend.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String username;
+  private String name;
+  private String email;
+
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+  
+}
